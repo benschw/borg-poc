2c0c7a02e502d645b524d87ff7e26f8304b3b14b
Ben Schwartz <benschw@gmail.com>
2015-11-29 08:21:43 -0600

    updates

--- src/Demo/Shaky/ShakyResource.php
+++ src/Demo/Shaky/ShakyResource.php
@@ -31,14 +31,13 @@ class ShakyResource {
 	public function test(GetParam $msg) {
 		$words = $this->coll()->mkchan(Primitive::getClass());
 
-		for ($i = 0; $i < 10; $i++) {
+		for ($i = 0; $i < 5; $i++) {
 			$this->coll()->doTest(new Primitive($msg->get()." ".$i), $words);
 		}
 
 		$replies = [];
-		for ($i = 0; $i < 10; $i++) {
-			list($chId, $w) = $words->get();
-			$replies[] = $w->value();
+		for ($i = 0; $i < 5; $i++) {
+			$replies[] = $words->get()->value();
 		}
 		$words->close();
 
